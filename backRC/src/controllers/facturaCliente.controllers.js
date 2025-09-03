c414539c4e7a6cf129a4af7dfe07455cda554047
danidev-100 <adriandanielfernandez@gmail.com>
2025-09-03 20:33:22 -0300

    facturaTablas

--- backRC/src/controllers/facturaCliente.controllers.js
+++ backRC/src/controllers/facturaCliente.controllers.js
@@ -1,10 +1,10 @@
 import { prisma } from "../../db.js";
 
 export const createFacturaCliente = async (req, res) => {
-  const { name, valor, efectivo, cheque, dolar, cotizacionDolar, estado, clienteId} =
+  const { name, valor, efectivo, cheque, dolar, cotizacionDolar, estado,clienteId } =
     req.body;
 
-       console.log(req.body);
+      
   try {
     const newFacturaCliente = await prisma.facturaCliente.create({
       data: {
@@ -15,16 +15,13 @@ export const createFacturaCliente = async (req, res) => {
         dolar,
         cotizacionDolar,
         estado,
-        clienteId
-        // Conectar con el cliente existente con id 1
+      cliente: { connect: { id: clienteId } }
+       
       },
-      
-      include: {
-        cliente: true
-      }
+     
     });
  
-    
+   
 
     res.json({
       message: "Factura creada y asignada al cliente correctamente",
